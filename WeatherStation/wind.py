f1653aea5b7679fbc6d6948daa0966b963d3c69c
Jamie Clayton <Jclayton@ermpower.com.au>
2021-05-09 19:44:05 +1000

    Add some weather variables

---
+++ WeatherStation/wind.py
@@ -0,0 +1,11 @@
+from gpiozero import Button
+
+wind_speed_sensor = Button(5)
+wind_count = 0
+
+def spin():
+    global wind_count
+    wind_count = wind_count + 1
+    print("spin" + str(wind_count))
+
+wind_speed_sensor.when_pressed = spin
