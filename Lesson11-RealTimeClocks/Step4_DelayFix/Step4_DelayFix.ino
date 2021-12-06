a6ae17739716d98f90d72a5ba0b5da321a3d344e
Jamie Clayton <jamie@jenasys.com>
2021-12-06 17:52:49 +1000

    Change the code to use methods
    * Reduce the noise in the code
    * Swap to a single relay communications pin

--- Lesson11-RealTimeClocks/Step4_DelayFix/Step4_DelayFix.ino
+++ Lesson11-RealTimeClocks/Step4_DelayFix/Step4_DelayFix.ino
@@ -1,24 +1,31 @@
 #include <DS3232RTC.h>
 
-// Real Time Clock Hardware Library :- https://github.com/hideakitai/DS323x
+// Real Time Clock Hardware Library :- https://github.com/JChristensen/DS3232RTC
+// Sub Second resolutions for DS3231 RTC: https://stackoverflow.com/questions/44644383/how-to-get-millisecond-resolution-from-ds3231-rtc
 
 DS3232RTC myRTC(false);     // tell constructor not to initialize the I2C bus (Wiring option)
 unsigned long myTime;       // Enable us to monitor milli seconds. Generally, you should use "unsigned long" for variables that hold time as it will 
 bool sentSignal = false;
 
-int relay1 = 7;
-int relay2 = 5;
-int relayOnTimeInMilliseconds = 100;   
-
+const int relay1 = 7;
+const unsigned long relayOnTimeInMilliseconds = 100;   
 
 void setup() {
   Serial.begin(9600); // open the serial port at 9600 bps:
   
   // put your setup code here, to run once:
   myRTC.begin(); 
+
+  //  Manually Set the ardunio board time - Allow about 15 second for the compile and upload process.
+  //  22h00m00s on 5Dec2021
+  setTime(22, 00, 00, 05, 12, 2021);
   
+  // following line sets the RTC to the date & time from the ardunio
+  // RTC do not function correctly until you set the date and time.
+  myRTC.set(now());
+
+  //  Setup the ardunio board pin for the relay.
   pinMode(relay1, OUTPUT);
-  pinMode(relay2, OUTPUT);
 }
 
 void loop() {
@@ -27,30 +34,52 @@ void loop() {
   myRTC.read(tm);
   unsigned long currentMillis = millis();
   
+//  Serial.print("Real Time: ");
+//  Serial.print(tm.Minute);
+//  Serial.print(":");
+//  Serial.print(tm.Second);
+//  Serial.print(" ");
+//  Serial.print(currentMillis); 
+//  Serial.println("ms"); 
+  
   // Record the top and bottom of a second hand passing the 0/360 degree and 180 degree of the rotation through the circle.
   if(tm.Second == 30 || tm.Second == 0){   
     if (!sentSignal){
-      if (tm.Second == 30) {
-        Serial.println("Relay 1 on");
-        digitalWrite(relay1, HIGH);
-        delay(relayOnTimeInMilliseconds);
-        digitalWrite(relay1, LOW);
-        Serial.println("1 off");
-      }else{
-        Serial.println("Relay 2");
-        digitalWrite(relay2, HIGH);
-        delay(relayOnTimeInMilliseconds);
-        digitalWrite(relay2, LOW);
-        Serial.println("2 off");
-      }
+      SetRelay(relay1,HIGH);
       sentSignal = true;
-    }    
-  }else if(tm.Second != 30 && tm.Second != 0)
-  {
-    //  Reset the signal after once out time moves to the next second.
-    sentSignal = false;
-    // digitalWrite(relay1, LOW);
-    // digitalWrite(relay2, LOW);
+      
+      //   Delay is bad and using the ardunio board time not the RTC.
+      delay(relayOnTimeInMilliseconds);
+    }
+    
+    // Logic to switch the Relay pulse off as long as the relayOnTimeInMilliseconds has also been exceeded
+    if (sentSignal && digitalRead(relay1)== HIGH){             
+      SetRelay(relay1,LOW);
+    }      
   }
   
+  if(tm.Second != 30 && tm.Second != 0)
+  {
+    //  Reset the signal after once out time moves to the next second.
+    sentSignal = false;  
+    
+    //  In the rare case something has gone wrong, lets fix it.
+    if (digitalRead(relay1)== HIGH){             
+      SetRelay(relay1,LOW);
+    }      
+  }
+}
+
+void SetRelay(int relayControlPin, uint8_t highOrLow){
+  if (highOrLow == HIGH || highOrLow == LOW){
+    Serial.print("Relay on pin ");
+    Serial.print(relayControlPin);
+    if (highOrLow == HIGH){ 
+      Serial.println(" on");
+    }
+    if (highOrLow == LOW){          
+      Serial.println(" off");
+    }
+    digitalWrite(relayControlPin, highOrLow);  
+  }  
 }
